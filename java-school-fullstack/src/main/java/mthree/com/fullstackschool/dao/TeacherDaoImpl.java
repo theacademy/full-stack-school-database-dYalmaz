16302065bb2296b14ce16c3eabcab462b6e83e14
dYalmaz <120399269+dYalmaz@users.noreply.github.com>
2026-05-26 17:34:00 +0100

    teacherDao

--- java-school-fullstack/src/main/java/mthree/com/fullstackschool/dao/TeacherDaoImpl.java
+++ java-school-fullstack/src/main/java/mthree/com/fullstackschool/dao/TeacherDaoImpl.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.List;
@@ -23,7 +24,25 @@ public class TeacherDaoImpl implements TeacherDao {
     public Teacher createNewTeacher(Teacher teacher) {
         //YOUR CODE STARTS HERE
 
-        return null;
+        final String sql = "INSERT INTO teacher(tFName, tLName, dept) VALUES(?,?,?);";
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update((Connection conn) -> {
+
+            PreparedStatement statement = conn.prepareStatement(
+                    sql,
+                    Statement.RETURN_GENERATED_KEYS);
+
+            statement.setString(1, teacher.getTeacherFName());
+            statement.setString(2, teacher.getTeacherLName());
+            statement.setString(3, teacher.getDept());
+            return statement;
+
+        }, keyHolder);
+
+        teacher.setTeacherId(keyHolder.getKey().intValue());
+
+        return teacher;
 
         //YOUR CODE ENDS HERE
     }
@@ -32,7 +51,8 @@ public class TeacherDaoImpl implements TeacherDao {
     public List<Teacher> getAllTeachers() {
         //YOUR CODE STARTS HERE
 
-        return null;
+        final String sql = "SELECT tid, tFName, tLName, dept FROM teacher;";
+        return jdbcTemplate.query(sql, new TeacherMapper());
 
         //YOUR CODE ENDS HERE
     }
@@ -41,7 +61,10 @@ public class TeacherDaoImpl implements TeacherDao {
     public Teacher findTeacherById(int id) {
         //YOUR CODE STARTS HERE
 
-        return null;
+        final String sql = "SELECT tid, tFName, tLName, dept "
+                + "FROM teacher WHERE tid = ?;";
+
+        return jdbcTemplate.queryForObject(sql, new TeacherMapper(), id);
 
         //YOUR CODE ENDS HERE
     }
@@ -50,6 +73,17 @@ public class TeacherDaoImpl implements TeacherDao {
     public void updateTeacher(Teacher t) {
         //YOUR CODE STARTS HERE
 
+        final String sql = "UPDATE teacher SET "
+                + "tFName = ?, "
+                + "tLName = ?, "
+                + "dept = ? "
+                + "WHERE tid = ?;";
+
+        jdbcTemplate.update(sql,
+                t.getTeacherFName(),
+                t.getTeacherLName(),
+                t.getDept(),
+                t.getTeacherId());
 
         //YOUR CODE ENDS HERE
     }
@@ -58,6 +92,8 @@ public class TeacherDaoImpl implements TeacherDao {
     public void deleteTeacher(int id) {
         //YOUR CODE STARTS HERE
 
+        final String sql = "DELETE FROM teacher WHERE tid = ?;";
+        jdbcTemplate.update(sql, id);
 
         //YOUR CODE ENDS HERE
     }
